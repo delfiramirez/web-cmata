5defd285d8753aa8163ff89173a204bed9dc6752
delfin@segonquart.net <delfin@segonquart.net>
2014-10-21 17:47:12 +0200

    index
    main index

---
+++ inc/constants.php
@@ -0,0 +1,16 @@
+<?php
+
+$BASEURL="http://carlosmata.eu";
+
+$title="Carlos Mata";
+
+$titleActivity="Sculptures and Bijoux";
+
+$webTitle="Official site of Carlos Mata";
+
+$description="Carlos Mata Scuptures and Bijoux";
+
+$keywords=array('carlos', 'mata', 'sculpteur', 'bijoux', 'catalogue', 'artist', 'marguerite', 'yarmats', 'segonquart', 'studio');
+
+$copyright="Segonquart Studio ".date('Y')." and Mr. Carlos Mata";
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'inc/constants.php' ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <?php include 'inc/part-header.php' ?>
+
+    <body>
+        <div id="info">
+            <h1><?php echo $title ?></h1>
+            <h2><?php echo $titleActivity ?></h2>
+            <h3><?php echo $webTitle ?></h3>
+        </div>
+        <div id="content">
+            <?php include 'inc/part-content.php' ?>
+        </div>
+    </body>
+</html>
