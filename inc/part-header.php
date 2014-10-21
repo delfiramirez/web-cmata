978748b3b2d03fc6630da9826d50a55c23345931
delfin@segonquart.net <delfin@segonquart.net>
2014-10-21 19:45:34 +0200

    revision
    add-ons and refinements

---
+++ inc/part-header.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * 
+ * 
+ * 
+ */
+?>
+
+<head>
+
+    <?php include '/' . 'part-header.php' ?>
+	
+    <?php include '/' . 'part-styles.php.inc' ?>
+	
+    <?php include '/' . 'part-script-libraries.php.inc' ?>
+	
+</head>
--- index.php
+++ index.php
@@ -1,13 +1,21 @@
+<?php
+/* 
+ * 
+ * 
+ * 
+ */
+?>
+
 <?php include 'inc' . '/' . 'constants.php' ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+	<base href="<?php echo $baseHref ?>">
     <?php include 'inc' . '/' . 'part-header.php' ?>
 
     <body>
         <div id="info">
-            <h1><?php echo $title ?></h1>
+            <h1><?php echo htmlspecialchars($title, ENT_NOQUOTES, 'utf-8'); ?></h1>
             <h2><?php echo $titleActivity ?></h2>
             <h3><?php echo $webTitle ?></h3>
         </div>
