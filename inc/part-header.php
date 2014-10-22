df4bd40e8aaf3740f86d83fa4caf7d3cc1c33c4f
delfin@segonquart.net <delfin@segonquart.net>
2014-10-22 03:22:30 +0200

    revision
    .inc files removed

--- inc/part-header.php
+++ inc/part-header.php
@@ -2,9 +2,9 @@
 
     <?php include '/' . 'part-header.php' ?>
 	
-    <?php include '/' . 'part-styles.php.inc' ?>
+    <?php include '/' . 'part-styles.php' ?>
 	
-    <?php include '/' . 'part-script-libraries.php.inc' ?>
+    <?php include '/' . 'part-script-libraries.php' ?>
 	
 </head>
 
