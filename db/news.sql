1042f6fbbde89d17266c381920cec5a9c82864b8
delfin@segonquart.net <delfin@segonquart.net>
2014-10-23 19:27:13 +0200

    db
    minor revision

---
+++ db/news.sql
@@ -0,0 +1,16 @@
+﻿USE cmata;
+
+DROP TABLE IF EXISTS 'noticias';
+
+CREATE TABLE 'noticias' (
+  'id_noticias' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  'titulo' VARCHAR ( 60 ) CHARACTER SET utf8 DEFAULT NULL,
+  'imagen' LONGBLOB NOT NULL,
+  'texto' VARCHAR ( 255 ) CHARACTER SET utf8 DEFAULT NULL,
+  'enlace' VARCHAR ( 255 ) NOT NULL,
+  created TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+LOAD XML LOCAL INFILE 'noticias.xml'
+    ->   INTO TABLE noticias
+    ->   ROWS IDENTIFIED BY '<registro>';
