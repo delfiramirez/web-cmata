f828c2f90cc60a9132512846a2ee45c03b0e106c
delfin@segonquart.net <delfin@segonquart.net>
2014-10-26 02:27:35 +0100

    db
    revision

--- db/appNews.sql
+++ db/appNews.sql
@@ -3,7 +3,6 @@
 DROP TABLE IF EXISTS 'noticias';
 
 CREATE TABLE 'noticias' (
-  'id_noticias' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   'titulo' VARCHAR ( 60 ) CHARACTER SET utf8 DEFAULT NULL,
   'imagen' LONGBLOB NOT NULL,
   'texto' VARCHAR ( 255 ) CHARACTER SET utf8 DEFAULT NULL,
@@ -13,4 +12,5 @@ CREATE TABLE 'noticias' (
 
 LOAD XML LOCAL INFILE '/assets/xml/noticias.xml'
 		 INTO TABLE noticias
-         ROWS IDENTIFIED BY '<registro>';
+         ROWS IDENTIFIED BY '<registro>'
+		 SET ID=NULL;
