940d11a15bd169c6e50bb88491bfdf86a60044f0
delfin@segonquart.net <delfin@segonquart.net>
2014-10-27 03:08:58 +0100

    db
    minor updates

---
+++ db/appMap.sql
@@ -0,0 +1,14 @@
+﻿USE cmata;
+
+DROP TABLE IF EXISTS 'mapsculpture';
+CREATE TABLE 'mapsculpture' (
+'id_map' INT ( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+'name' VARCHAR ( 35 ) CHARACTER SET utf8 DEFAULT NULL,
+'file' LONGBLOB NOT NULL,
+'url' VARCHAR ( 255 ) CHARACTER SET utf8 DEFAULT NULL,
+'location' VARCHAR ( 255 ) CHARACTER SET utf8 DEFAULT NULL,
+created TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+LOAD XML LOCAL INFILE '/assets/xml/mapxml.xml'
+INTO TABLE noticias
+ROWS IDENTIFIED BY '<sculpture>';
