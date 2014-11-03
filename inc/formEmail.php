ca83aa8d65520a125429fe1022cb2b62e488af3d
delfin@segonquart.net <delfin@segonquart.net>
2014-11-03 20:26:41 +0100

    revision
    minor updates

--- inc/formEmail.php
+++ inc/formEmail.php
@@ -1,22 +1,22 @@
 <?php
 
-$headers = "MIME-Version: 1.0\r\n";
+$headers = "MIME-Version: 1.0" . "\r\n";
 
-$headers = "From: " . stripslashes(trim( $_POST["firstName"] )) ." ". stripslashes(trim($_POST["lastname"] )). "<" . stripslashes(trim($_POST["email"] )).">\r\n";
+$headers = "From: " . stripslashes(trim( $_POST["firstName"] )) ." ". stripslashes(trim($_POST["lastname"] )). "<" . stripslashes(trim($_POST["email"] )) . ">\r\n";
 
 $headers .= "Reply-To: " . stripslashes(trim($_POST["email"] )) . "\r\n";
 
-$headers .= "Reply-To: no-reply@carlosmata.eu\r\n";
+$headers .= "Reply-To: no-reply@carlosmata.eu" . "\r\n";
 
 $headers .= "Return-path: " . stripslashes(trim($_POST["email"]));
 	   
-$headers .= "Content-type: text/plain; charset=utf-8";
+$headers .= "Content-type: text/plain; charset=utf-8" . "\r\n";
 	   
-$headers .= "Last-Modified: " . date("D, d M Y H:i:s");
+$headers .= "Last-Modified: " . date('D, d M Y H:i:s');
 	   
-$headers .= "Pragma: public";
+$headers .= "Pragma: public" . "\r\n";
 	   
-$headers .= "Cache-Control: cache, must-revalidate";
+$headers .= "Cache-Control: cache, must-revalidate" . "\r\n";
 
 $sendTo = strtolower(trim("admin@carlosmata.eu");
 
--- inc/formRecommend.php
+++ inc/formRecommend.php
@@ -4,48 +4,48 @@
 
        $subject = "Carlos Mata Sculpteur Website";
 
-       $mailheaders = "From: <$senderName> \n";
+       $mailheaders = "From: <$senderName>" . "\n";
 
-       $mailheaders .= "Reply-To: <$senderEmail>\n\n";
+       $mailheaders .= "Reply-To: <$senderEmail>" . "\n\n";
 	   
-	   $mailheaders .= "Content-type: text/plain; charset=utf-8";
+	   $mailheaders .= "Content-type: text/plain; charset=utf-8" . "\r\n";
 	   
-	   $mailheaders .= "Last-Modified: " . date("D, d M Y H:i:s");
+	   $mailheaders .= "Last-Modified: " . date('D, d M Y H:i:s');
 	   
-	   $mailheaders .= "Pragma: public";
+	   $mailheaders .= "Pragma: public" . "\r\n";
 	   
-	   $mailheaders .= "Cache-Control: cache, must-revalidate";
+	   $mailheaders .= "Cache-Control: cache, must-revalidate" . "\r\n";
 	   
 
-		   $msg = "Carlos Mata is on-line || Visit his pieces and artwork \n";
+		   $msg = "Carlos Mata is on-line || Visit his pieces and artwork" . "\n";
 
            $msg .= "\n";
 
-		   $msg .= "$senderMessage\n";
+		   $msg .= "$senderMessage" . "\n";
 
            $msg .= "\n";
 
-           $msg .= "$senderName\n". "a personal honest recommendation";
+           $msg .= "$senderName\n". "a personal honest recommendation" . "\n";
 
        mail($recipient, $subject, $msg, $mailheaders);
 
-       $recipient = "admin@carlosmata.eu, webmaster@segonquart.net ";  
+       $recipient = "admin@carlosmata.eu, webmaster@segonquart.net";  
 
-       $subject = "$senderName\n". "has recommended you website";
+       $subject = "$senderName\n". "has recommended you website" . "\n";
 
-       $mailheaders = "From: <$senderName> \n";
+       $mailheaders = "From: <$senderName>"."\n";
 
-       $mailheaders .= "Reply-To: <$senderEmail>\n\n";
+       $mailheaders .= "Reply-To: <$senderEmail>" ."\n\n";
 	   
-	   $mailheaders .= "Content-type: text/plain; charset=utf-8";
+	   $mailheaders .= "Content-type: text/plain; charset=utf-8" . "\r\n";
 	   
 	   $mailheaders .= "Last-Modified: " . date("D, d M Y H:i:s");
 	   
-	   $mailheaders .= "Pragma: public";
+	   $mailheaders .= "Pragma: public" . "\r\n";
 	   
-	   $mailheaders .= "Cache-Control: cache, must-revalidate";
+	   $mailheaders .= "Cache-Control: cache, must-revalidate" . "\r\n";
 
-       $msg .= "$friendsEmail\n". "has received your invitation. Thank you very much";
+       $msg .= "$friendsEmail\n". "has received your invitation. Thank you very much"."\n";
 		   
    
 
