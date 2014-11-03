fcdf430494f4d954405bf131df85cd41a331303c
delfin@segonquart.net <delfin@segonquart.net>
2014-11-03 02:10:21 +0100

    revision
    minor sec updates

--- inc/formRecommend.php
+++ inc/formRecommend.php
@@ -8,8 +8,14 @@
 
        $mailheaders .= "Reply-To: <$senderEmail>\n\n";
 	   
-	   $mailheaders .= 'Content-type: text/plain; charset=utf-8';
-
+	   $mailheaders .= "Content-type: text/plain; charset=utf-8";
+	   
+	   $mailheaders .= "Last-Modified: " . date("D, d M Y H:i:s");
+	   
+	   $mailheaders .= "Pragma: public";
+	   
+	   $mailheaders .= "Cache-Control: cache, must-revalidate";
+	   
 
 		   $msg = "Carlos Mata is on-line || Visit his pieces and artwork \n";
 
@@ -19,7 +25,7 @@
 
            $msg .= "\n";
 
-           $msg .= "$senderName\n". "honest recommendation";
+           $msg .= "$senderName\n". "a personal honest recommendation";
 
        mail($recipient, $subject, $msg, $mailheaders);
 
@@ -30,13 +36,17 @@
        $mailheaders = "From: <$senderName> \n";
 
        $mailheaders .= "Reply-To: <$senderEmail>\n\n";
-
+	   
+	   $mailheaders .= "Content-type: text/plain; charset=utf-8";
+	   
+	   $mailheaders .= "Last-Modified: " . date("D, d M Y H:i:s");
+	   
+	   $mailheaders .= "Pragma: public";
+	   
+	   $mailheaders .= "Cache-Control: cache, must-revalidate";
 
        $msg .= "$friendsEmail\n". "has received your invitation. Thank you very much";
-
-           mail($recipient, $subject, $msg, $mailheaders);
 		   
-	   $msg="";
-	   
+   
 
 ?>
